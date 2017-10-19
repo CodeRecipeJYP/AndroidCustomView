afbce5feea295561da9b690ad0363ace7950dcb3
pjo901018 <pjo901018@korea.ac.kr>
2017-10-19 13:23:01 +0900

    Adding ChartView

---
+++ app/src/main/java/com/asuscomm/yangyinetwork/customview/ChartView.java
@@ -0,0 +1,16 @@
+package com.asuscomm.yangyinetwork.customview;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+
+/**
+ * Created by jaeyoung on 19/10/2017.
+ */
+
+public class ChartView extends View {
+    public ChartView(Context context) {
+        super(context);
+        setBackgroundColor(Color.RED);
+    }
+}
--- app/src/main/java/com/asuscomm/yangyinetwork/customview/MainActivity.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/customview/MainActivity.java
@@ -2,6 +2,7 @@ package com.asuscomm.yangyinetwork.customview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import java.io.InputStream;
 import java.util.List;
@@ -19,5 +20,9 @@ public class MainActivity extends AppCompatActivity {
         for (StockData stockData : data) {
             System.out.println(stockData.toString());
         }
+
+        ChartView chartView = new ChartView(this);
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
+        relativeLayout.addView(chartView);
     }
 }
