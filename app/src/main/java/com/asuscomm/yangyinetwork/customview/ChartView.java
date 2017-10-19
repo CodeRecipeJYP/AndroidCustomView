10aa99168898c771ed5cc11a6a56aa0154a81309
pjo901018 <pjo901018@korea.ac.kr>
2017-10-19 13:43:35 +0900

    Adding updateMaxAndMin

--- app/src/main/java/com/asuscomm/yangyinetwork/customview/ChartView.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/customview/ChartView.java
@@ -17,7 +17,7 @@ public class ChartView extends View {
     List<StockData> data;
     List<StockData> subset;
 
-    float width, height;
+    float width, height, maxPrice, minPrice;
     Paint paint = new Paint();
 
     public ChartView(Context context, int resId) {
@@ -26,7 +26,7 @@ public class ChartView extends View {
 
         InputStream inputStream = getResources().openRawResource(resId);
         data = CSVParser.read(inputStream);
-        subset = data;
+        showLast();
         paint.setColor(Color.GREEN);
     }
 
@@ -39,10 +39,26 @@ public class ChartView extends View {
 
         for (int i = subset.size() - 1; i >= 0; i--) {
             StockData stockData = subset.get(i);
-            canvas.drawRect(left, height - stockData.high, left + rectWidth, height - stockData.low, paint);
+            canvas.drawRect(left, getYPosition(stockData.high), left + rectWidth, getYPosition(stockData.low), paint);
             left += rectWidth;
         }
 //        canvas.drawRect(0, height / 2, width, 0, paint);
+    }
+
+    private float getYPosition(float price) {
+        float scaleFactorY = (price - minPrice) / (maxPrice - minPrice);
+        return height - height * scaleFactorY;
+    }
+
+    private void updateMaxAndMin() {
+        maxPrice = -1f;
+        minPrice = 99999f;
+        for (StockData stockData : subset) {
+            if (stockData.high > maxPrice)
+                maxPrice = stockData.high;
+            if (stockData.low < minPrice)
+                minPrice = stockData.low;
+        }
     }
 
     public void showLast() {
@@ -51,6 +67,7 @@ public class ChartView extends View {
 
     public void showLast(int n) {
         subset = data.subList(0, n);
+        updateMaxAndMin();
         // notify
         invalidate();
     }
